510b90a214598facddb4b6c5e0853ae68afa64fa
wahaha <me@wahaha.com>
2024-01-18 10:48:59 +0800

    :tada: init

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/jiebutech/log
+
+go 1.21.5
+
+require (
+	github.com/google/uuid v1.5.0
+	github.com/jiebutech/config v1.0.0
+	go.uber.org/zap v1.26.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+)
+
+require (
+	github.com/spf13/cast v1.6.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ log.go
@@ -0,0 +1,201 @@
+package log
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/jiebutech/config"
+	"github.com/jiebutech/log/notify"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"os"
+	"path/filepath"
+)
+
+type eaLogger struct {
+	zapLogger *zap.Logger
+	notifier  notify.Notifier
+}
+
+var logger *eaLogger
+
+const (
+	logIdKey  = "log-id"
+	userIdKey = "user-id"
+	uriKey    = "uriKey"
+
+	defaultLogPath = "./logs"
+)
+
+func init() {
+	log, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	logger = &eaLogger{zapLogger: log}
+}
+
+type apiCtx interface {
+	AuthUserID() int
+	RequestURI() string
+}
+
+type debugLevelEnabler string
+
+func (e debugLevelEnabler) Enabled(level zapcore.Level) bool {
+	return level <= zapcore.DebugLevel
+}
+
+type infoLevelEnabler string
+
+func (e infoLevelEnabler) Enabled(level zapcore.Level) bool {
+	return level > zapcore.DebugLevel && level < zapcore.ErrorLevel
+}
+
+type errorLevelEnabler string
+
+func (e errorLevelEnabler) Enabled(level zapcore.Level) bool {
+	return level >= zapcore.ErrorLevel
+}
+
+// Configure 配置log
+func Configure(cfg *config.Configuration) error {
+	if cfg.App.Debug {
+		zapLogger, err := zap.NewDevelopment()
+		if err != nil {
+			return err
+		}
+		logger.zapLogger = zapLogger
+	} else {
+		debugWs, err := getLogWriter(*cfg.Log, "debug")
+		if err != nil {
+			return err
+		}
+		infoWs, err := getLogWriter(*cfg.Log, "info")
+		if err != nil {
+			return err
+		}
+		errorWs, err := getLogWriter(*cfg.Log, "error")
+		if err != nil {
+			return err
+		}
+		prodconf := zap.NewProductionEncoderConfig()
+		// 实例化日志器
+		debugCore := zapcore.NewCore(zapcore.NewJSONEncoder(prodconf), debugWs, debugLevelEnabler(""))
+		infoCore := zapcore.NewCore(zapcore.NewJSONEncoder(prodconf), infoWs, infoLevelEnabler(""))
+		errCore := zapcore.NewCore(zapcore.NewJSONEncoder(prodconf), errorWs, errorLevelEnabler(""))
+
+		zapLogger := zap.New(zapcore.NewTee(debugCore, infoCore, errCore))
+		logger.zapLogger = zapLogger
+	}
+	if cfg.Log.DingtalkAlarm != nil {
+		logger.notifier = notify.NewDingtalkNotifyClient(cfg.Log.DingtalkAlarm.AccessToken, cfg.Log.DingtalkAlarm.SignSecret)
+	}
+	return nil
+}
+
+type setter interface {
+	Set(key string, value interface{})
+}
+
+// getLogWriter 获取日志输出方式  日志文件 控制台
+func getLogWriter(conf config.LogConfig, level string) (zapcore.WriteSyncer, error) {
+	if conf.Driver == config.StdoutDriver {
+		// 日志同时输出到控制台
+		return zapcore.AddSync(os.Stdout), nil
+	}
+
+	// 判断日志路径是否存在，如果不存在就创建
+	if exist := IsExist(conf.Path); !exist {
+		if conf.Path == "" {
+			conf.Path = defaultLogPath
+		}
+		if err := os.MkdirAll(conf.Path, os.ModePerm); err != nil {
+			// 指定目录失败， 则写在默认目录
+			conf.Path = defaultLogPath
+			if err := os.MkdirAll(conf.Path, os.ModePerm); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	// 日志文件 与 日志切割 配置
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   filepath.Join(conf.Path, level+".log"), // 日志文件路径
+		MaxSize:    conf.LogFileMaxSize,                    // 单个日志文件最大多少 mb
+		MaxBackups: conf.LogFileMaxBackups,                 // 日志备份数量
+		MaxAge:     conf.LogMaxAge,                         // 日志最长保留时间
+		Compress:   true,                                   // 是否压缩日志
+	}
+	// 日志只输出到日志文件
+	return zapcore.AddSync(lumberJackLogger), nil
+}
+
+// IsExist 判断文件或者目录是否存在
+func IsExist(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil || os.IsExist(err)
+}
+
+// InjectLogID 注入logid到上下文中
+func InjectLogID(ctx setter) {
+	logIdVal, _ := uuid.NewUUID()
+	ctx.Set(logIdKey, logIdVal)
+}
+
+func injectCommonValue(ctx context.Context) *zap.Logger {
+	ac, ok := ctx.(apiCtx)
+
+	l := logger.zapLogger.With(zap.Any(logIdKey, ctx.Value(logIdKey)))
+	if ok {
+		l = l.With(zap.Any(userIdKey, ac.AuthUserID()), zap.Any(uriKey, ac.RequestURI()))
+	}
+	return l
+}
+
+// Debug 输出Debug日志
+func Debug(ctx context.Context, msg string, fields ...zap.Field) {
+	injectCommonValue(ctx).Debug(msg, fields...)
+}
+
+// Info 输出Info日志
+func Info(ctx context.Context, msg string, fields ...zap.Field) {
+	injectCommonValue(ctx).Info(msg, fields...)
+}
+
+// Warn 输出Warn日志
+func Warn(ctx context.Context, msg string, fields ...zap.Field) {
+	injectCommonValue(ctx).Warn(msg, fields...)
+}
+
+// Error 输出error日志
+func Error(ctx context.Context, msg string, fields ...zap.Field) {
+	injectCommonValue(ctx).Error(msg, fields...)
+}
+
+// Panic 输出Panic日志
+func Panic(ctx context.Context, msg string, fields ...zap.Field) {
+	injectCommonValue(ctx).Panic(msg, fields...)
+}
+
+const alarmTemplate = `
+| Title     | %s   |
+| -------- | ---- |
+| Request-ID | %s |
+| Message | %s |
+| Context | %s |
+`
+
+// Alarm 通知
+func Alarm(ctx context.Context, title, msg string, reqCtx interface{}, at ...string) {
+	if logger.notifier == nil {
+		return
+	}
+	go func() {
+		logContext, _ := json.Marshal(reqCtx)
+		content := fmt.Sprintf(alarmTemplate, title, ctx.Value(logIdKey), msg, string(logContext))
+		_ = logger.notifier.SendMarkdown(title, content, at...)
+	}()
+}
